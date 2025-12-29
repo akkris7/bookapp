414cc1efcf656347af0b34e6c8608f7d99da5b56
akkris7 <akkris7@outlook.com>
2025-12-30 01:22:18 +0530

    Initial backend setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Bookapp API running");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
